ade5f81fee00b5f124737d1c185c305a369fe03d
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-01-13 21:14:59 +0200

    hw2

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import {useEffect, useState} from 'react';
+// import {useEffect, useState} from 'react';
 import {userService} from '../'
-
+use
 
 const Users = () => {
     const [users, setUsers] = useState([]);
