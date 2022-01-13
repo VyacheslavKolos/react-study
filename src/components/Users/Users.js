4dc33fd33f0143b84646e371b5f47359a5eb4fa5
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-01-13 17:28:27 +0200

    hw2

---
+++ src/components/Users/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users = () => {
+    const [users,setUsers]=useState([]);
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
