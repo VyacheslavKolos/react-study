98494bd8b2f143887a881f4f06e3084644274dab
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-02-09 11:29:38 +0200

    hw-7

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,10 +1,11 @@
 import {axiosService} from "./axios.service";
 
+
 import {ICar} from "../interfaces";
 import {urls} from "../constants";
 
-export const carService={
-    getAll:()=>axiosService.get<ICar[]>(urls.cars),
-    create:(car:ICar)=>axiosService.post<ICar>(urls.cars,car),
-    deleteById:(id:number)=>axiosService.delete<void>(`${urls.cars}/${id}`)
+export const carService = {
+    getAll: () => axiosService.get<ICar[]>(urls.cars),
+    create: (car: ICar) => axiosService.post<ICar>(urls.cars, car),
+    deleteById: (id: number) => axiosService.delete<void>(`${urls.cars}/${id}`)
 }
